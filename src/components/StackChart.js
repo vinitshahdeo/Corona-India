ffbf19f3e9e4017f5db831ce058cefdc9bb635a2
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-03-28 18:23:47 +0530

    fixed the number of deaths, changed the API source

--- src/components/StackChart.js
+++ src/components/StackChart.js
@@ -34,14 +34,15 @@ export default class StackChart extends React.Component {
       state = [],
       deaths = [];
     fetch(
-      "https://sharadcodes.github.io/covid19-indian-stats/indian_states.json"
+      "https://api.covid19india.org/data.json"
     )
       .then((res) => res.json())
       .then((data) => {
+        data = data.statewise.splice(1);
         for (index = 0; index < data.length; index++) {
           if (parseInt(data[index].deaths) !== 0) {
             deaths.push(parseInt(data[index].deaths));
-            state.push(data[index].state_ut);
+            state.push(data[index].state);
           }
         }
         console.log(deaths);
