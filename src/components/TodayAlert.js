243e159e6f9103e949b861165dcb28e2f3d41647
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-03-30 20:53:30 +0530

    added donation button

--- src/components/TodayAlert.js
+++ src/components/TodayAlert.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Alert, Button, Container, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 /**
  * @author Vinit Shahdeo
@@ -28,7 +30,7 @@ export default class TodayAlert extends React.Component {
   render() {
     if (this.state.show) {
       return (
-        <Container>
+        <Container className="today-alert">
           <Alert
             variant="danger"
             onClose={() => this.setShow(false)}
@@ -40,7 +42,22 @@ export default class TodayAlert extends React.Component {
               <strong>{this.state.todayCases}</strong> and total recovered cases
               today is <strong>{this.state.todayRecovered}</strong>. The{" "}
               <strong>lockdown</strong> has been put in place to control the
-              situation, please follow this strictly.
+              situation, please follow this strictly. Kindly{" "}
+              <a
+                href="https://pib.gov.in/PressReleseDetailm.aspx?PRID=1608851"
+                target="_blank"
+              >
+                contribute
+              </a>{" "}
+              to <strong>PM CARES</strong> fund.
+              <span className="google-pay">
+                <Button
+                  href={"https://gpay.app.goo.gl/277dRg"}
+                  variant="primary"
+                >
+                  <FontAwesomeIcon icon={faGoogle} /> Donate Now
+                </Button>
+              </span>
             </p>
           </Alert>
         </Container>
