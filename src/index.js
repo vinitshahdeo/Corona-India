506923144bb1fc53694c5d8cb4876fa6a6d329af
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-04-08 01:09:17 +0530

    added stay safe, stay home banner in console

--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * 
+ * @author Vinit Shahdeo <vinitshahdeo@gmail.com>
+ */
+
 // being a bit cool :p
 console.log(
-  `%cHi I am Vinit Shahdeo!\nSuper glad to see you here.\nHope you are staying safe at home.\nFind me on GitHub (@vinitshahdeo).\nConsider leaving star on my repositories.`,
+  `%cHi, I am Vinit Shahdeo!\nSuper glad to see you here.\nHope you are staying safe at home.\nFind me on GitHub (@vinitshahdeo)!\nConsider leaving star on my repositories.`,
   'color: yellow; background:black; font-size: 24pt; font-weight: bold',
 )
+
+var styles = [
+  'background: linear-gradient(#D33106, #571402)'
+  , 'border: 4px solid #3E0E02'
+  , 'color: white'
+  , 'display: block'
+  , 'text-shadow: 0 2px 0 rgba(0, 0, 0, 0.3)'
+  , 'box-shadow: 0 2px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+  , 'line-height: 40px'
+  , 'text-align: center'
+  , 'font-weight: bold'
+  , 'font-size: 32px'
+].join(';');
+
+console.log('%c Stay Home, Stay Safe!', styles);
 
 ReactDOM.render(
   <React.StrictMode>
