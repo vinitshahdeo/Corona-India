bd45716a924e261a7318cac992def3ce215a8aa1
Vinit Shahdeo <vinit.shahdeo@getpostman.com>
2020-04-07 22:40:38 +0530

    added a console log for publicity

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// being a bit cool :p
+console.log(
+  `%cHi I am Vinit Shahdeo!\nSuper glad to see you here.\nHope you are staying safe at home.\nFind me on GitHub (@vinitshahdeo).\nConsider leaving star on my repositories.`,
+  'color: yellow; background:black; font-size: 24pt; font-weight: bold',
+)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
